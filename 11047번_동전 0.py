18e777675dadcabb93ea024572a031d3dcaedc97
이동현 <jsleeg7279@naver.com>
2021-01-08 10:31:36 +0900

    11047번_동전 0 2021/01/07

--- 11047번_동전 0.py
+++ 11047번_동전 0.py
@@ -10,15 +10,25 @@ for i in range(N):
     temp = int(sys.stdin.readline())
     coins.append(temp)
 
+# 동전 리스트 내림차순 정렬
+coins.reverse()
+
 # 동전 개수 초기화
 count = 0
 
-for i in range(-1, -N-1, -1):
-    if K >= coins[i]:
-        count += K // coins[i]
-        K = K % coins[i]
-    if K == 0:
-        break
+for i in coins:
+    if K >= i: 
+        count += K // i
+        K = K % i
+
+
+# 큰 동전부터 반복문 수행
+# for i in range(-1, -N-1, -1):
+#     if K >= coins[i]: 
+#         count += K // coins[i]
+#         K = K % coins[i]
+#     if K == 0: # 필요한 동전을 모두 센 경우
+#         break
 
 print(count)
 
