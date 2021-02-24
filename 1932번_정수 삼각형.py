3632e7401d1c2dce741b47de1989456410bba9ce
이동현 <jsleeg7279@naver.com>
2021-02-24 09:10:47 +0900

    1932번_정수 삼각형 2021/02/23

--- 1932번_정수 삼각형.py
+++ 1932번_정수 삼각형.py
@@ -11,10 +11,13 @@ for _ in range(N):
 
 for i in range(N - 1):
     for j in range(len(numbers[i + 1])):
+        # 맨 앞은 맨 앞끼리 합
         if j == 0:
             numbers[i + 1][j] = numbers[i][j] + numbers[i + 1][j]
+        # 맨 뒤는 맨 뒤끼리 합
         elif j == len(numbers[i + 1]) - 1:
             numbers[i + 1][-1] = numbers[i][-1] + numbers[i + 1][-1]
+        # 중간은 최대 값으로 저장
         else:
             numbers[i + 1][j] = max(numbers[i][j - 1] + numbers[i + 1][j], numbers[i][j] + numbers[i + 1][j])
     
